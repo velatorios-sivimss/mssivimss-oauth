3a64a66ee6b43c8290f1f38b81774c9ebd6bfd07
Irwin <irwin.ceballos@people-media.com.mx>
2023-05-17 15:50:31 -0600

    Arreglo Recuperar contraseña

--- src/main/java/com/imss/sivimss/oauth/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/imss/sivimss/oauth/service/impl/UsuarioServiceImpl.java
@@ -4,17 +4,25 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.imss.sivimss.oauth.beans.Usuario;
 import com.imss.sivimss.oauth.exception.BadRequestException;
 import com.imss.sivimss.oauth.service.UsuarioService;
+import com.imss.sivimss.oauth.util.LogUtil;
 import com.imss.sivimss.oauth.util.MensajeEnum;
 
 @Service
 public class UsuarioServiceImpl extends UtileriaService implements UsuarioService {
+	
+	@Autowired
+	private LogUtil logUtil;
+	
+	private static final String CONSULTA = "consulta";
 	
 	@SuppressWarnings("unchecked")
 	@Override
@@ -22,7 +30,11 @@ public class UsuarioServiceImpl extends UtileriaService implements UsuarioServic
 		
 		Usuario usuario= new Usuario();
 		List<Map<String, Object>> mapping;
-		List<Map<String, Object>> datos = consultaGenericaPorQuery( usuario.buscarUsuario(user) );
+		String query = usuario.buscarUsuario(user);
+		
+		logUtil.crearArchivoLog(Level.INFO.toString(),this.getClass().getSimpleName(),this.getClass().getPackage().toString(),"",CONSULTA+" "+ query);
+		
+		List<Map<String, Object>> datos = consultaGenericaPorQuery( query );
 		
 		mapping = Arrays.asList(modelMapper.map(datos, HashMap[].class));
 		
